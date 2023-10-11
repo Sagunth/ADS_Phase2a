8b32ec2a3e8a582b6ee36ed46772729775027bdf
M.Sagunthaladevi <146872025+Sagunth@users.noreply.github.com>
2023-10-11 07:44:18 -0700

    ADS_phase2

---
+++ sketch.ino
@@ -0,0 +1,26 @@
+int red = 2;
+int yellow= 4;
+int green= 12;
+
+void setup() {
+
+  pinMode(red, OUTPUT);
+  digitalWrite(red, LOW);
+  pinMode(yellow, OUTPUT);
+  digitalWrite(yellow, LOW);
+  pinMode(green, OUTPUT);
+  digitalWrite(green, LOW);
+
+}
+
+void loop() {
+  digitalWrite(red, HIGH);
+  delay(5000);
+  digitalWrite(red, LOW);
+  digitalWrite(yellow, HIGH);
+  delay(5000);
+  digitalWrite(yellow, LOW);
+  digitalWrite(green, HIGH);
+  delay(5000);
+  digitalWrite(green, LOW);
+}
